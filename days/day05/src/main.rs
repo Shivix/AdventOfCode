c17e67cac5721d04fde7c74f7c54d5efaa0fb42a
Mark Oborne <markoborne1@gmail.com>
2022-12-05 23:09:27 +0000

    chore(day05): improved stack parsing

--- days/day05/src/main.rs
+++ days/day05/src/main.rs
@@ -4,35 +4,11 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
     let mut stacks = vec![Vec::<char>::new(); 9];
     let (crates, instructions) = include_str!("../input").split_once("\n\n").unwrap();
-
     crates.lines().rev().for_each(|line| {
-        let line = line.as_bytes();
-        if line[1] > 57 {
-            stacks[0].push(line[1] as char);
-        }
-        if line[5] > 57 {
-            stacks[1].push(line[5] as char);
-        }
-        if line[9] > 57 {
-            stacks[2].push(line[9] as char);
-        }
-        if line[13] > 57 {
-            stacks[3].push(line[13] as char);
-        }
-        if line[17] > 57 {
-            stacks[4].push(line[17] as char);
-        }
-        if line[21] > 57 {
-            stacks[5].push(line[21] as char);
-        }
-        if line[25] > 57 {
-            stacks[6].push(line[25] as char);
-        }
-        if line[29] > 57 {
-            stacks[7].push(line[29] as char);
-        }
-        if line[33] > 57 {
-            stacks[8].push(line[33] as char);
+        for (i, c) in line.char_indices().skip(1).step_by(4) {
+            if c != ' ' {
+                stacks[i / 4].push(c);
+            }
         }
     });
     let regex = Regex::new(r"(?P<amount>[0-9]+)[^0-9]+(?P<from>[0-9]+)[^0-9]+(?P<to>[0-9]+)")
@@ -60,7 +36,7 @@ fn main() {
             }
         }
     }
-    for stack in stacks.iter() {
+    for stack in stacks {
         print!("{}", stack.last().unwrap());
     }
 }
