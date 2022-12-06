413c11a5bc355adf0db9130cff9aa7ddf383bbce
Mark Oborne <markoborne1@gmail.com>
2022-12-06 21:02:06 +0000

    feat(aoclib): add rust version of aoclib

---
+++ aoclib/src/lib.rs
@@ -0,0 +1,23 @@
+pub fn parse_args(args: Vec<String>) -> Option<i32> {
+    if args.len() == 1 {
+        return None;
+    }
+    if args[1] == "--part1" || args[1] == "-1" {
+        return Some(1)
+    }
+    if args[1] == "--part2" || args[1] == "-2" {
+        return Some(2)
+    }
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = parse_args(vec!["bin".to_string(), "--part1".to_string()]);
+        assert_eq!(result, Some(1));
+    }
+}
