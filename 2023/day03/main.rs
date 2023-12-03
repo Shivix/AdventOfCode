916d02ddf5d269e9e4c51f7aece3afcc72ebb9d3
Mark Oborne <markoborne1@gmail.com>
2023-12-04 00:28:05 +0100

    chore: update day03 rust solution

--- 2023/day03/main.rs
+++ 2023/day03/main.rs
@@ -1,8 +1,9 @@
+use regex::Regex;
 use std::collections::HashMap;
 use std::collections::HashSet;
 
 fn is_symbol(c: char) -> bool {
-    c < '.' || c > '9' || c == '/'
+    !c.is_digit(10) && c != '.'
 }
 
 const WIDTH: usize = 140;
@@ -20,55 +21,53 @@ fn make_adjacent_positions(i: usize) -> [usize; 8] {
     ]
 }
 
-// Could we regex the lines of the file, get index of capture multiplied + line number * width to
-// get pos
 fn main() {
-    let schematic = include_str!("input").lines().flat_map(|line| line.chars());
-
-    let mut part_ref = HashMap::<usize, i32>::new();
-    let mut part_positions = Vec::<usize>::new();
-    let mut part_number = String::new();
-
-    schematic.clone().enumerate().for_each(|(pos, character)| {
-        if character.is_digit(10) {
-            part_number.push(character);
-            part_positions.push(pos);
-            return;
-        }
-        for p in part_positions.iter() {
-            part_ref.insert(*p, part_number.parse::<i32>().unwrap());
-        }
-        part_positions.clear();
-        part_number.clear();
-    });
-
-    let mut part1 = 0;
-    let mut part2 = 0;
-    let mut parts_counted = HashSet::<i32>::new();
-    for (i, char) in schematic.enumerate() {
-        // part 1
-        if !is_symbol(char) {
-            continue;
-        }
-        let mut first_part = 0;
-        for pos in make_adjacent_positions(i).iter() {
-            if part_ref.contains_key(pos) {
-                let part = part_ref[pos];
-                // N.B. No part numbers have more than one adjacent symbol
-                if parts_counted.insert(part) {
-                    part1 += part;
-                }
-                // part 2
-                if char == '*' {
-                    if first_part != 0 && part != first_part {
-                        part2 += first_part * part;
-                    }
-                    first_part = part;
+    let regex = Regex::new(r"[0-9]+").unwrap();
+    // Create map of parts where key is position in the schematic and value is the part id.
+    let part_ref = include_str!("input").lines().enumerate().fold(
+        HashMap::<usize, i32>::new(),
+        |mut acc, (line_n, line)| {
+            for capture in regex.find_iter(line) {
+                for i in capture.start()..capture.end() {
+                    acc.insert(line_n * WIDTH + i, capture.as_str().parse::<i32>().unwrap());
                 }
             }
-        }
-        parts_counted.clear();
-    }
+            acc
+        },
+    );
 
+    // Now when checking adjacent positions to symbols we can see if a part is there by searching
+    // the map.
+    let mut parts_counted = HashSet::<i32>::new();
+    let (part1, part2) = include_str!("input")
+        .lines()
+        .flat_map(|line| line.chars())
+        .enumerate()
+        .fold((0, 0), |(mut acc1, mut acc2), (i, char)| {
+            if !is_symbol(char) {
+                return (acc1, acc2);
+            }
+            let mut first_part = 0;
+            for pos in make_adjacent_positions(i).iter() {
+                if part_ref.contains_key(pos) {
+                    let part = part_ref[pos];
+                    // N.B. No part numbers have more than one adjacent symbol
+                    if parts_counted.insert(part) {
+                        acc1 += part;
+                    }
+                    // part2
+                    if char == '*' {
+                        let part = part_ref[pos];
+                        if first_part != 0 && part != first_part {
+                            acc2 += first_part * part;
+                        }
+                        first_part = part;
+                    }
+                }
+            }
+            parts_counted.clear();
+            (acc1, acc2)
+        });
     println!("part1: {}, part2: {}", part1, part2);
+    assert!(part1 == 536576 && part2 == 75741499);
 }
