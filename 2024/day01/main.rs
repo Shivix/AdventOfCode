16ad589c18311c9b715516389f6cf409062a1894
Mark Oborne <markoborne1@gmail.com>
2024-12-03 21:47:52 +0000

    feat(2024): add day01 and day02 Rust solutions

--- 2024/day01/main.rs
+++ 2024/day01/main.rs
@@ -1,3 +1,33 @@
 fn main() {
-    println!("Hello, world!");
+    let (mut list1, mut list2): (Vec<i32>, Vec<i32>) = include_str!("input")
+        .lines()
+        .map(|line| {
+            let (left, right) = line.split_once("   ").unwrap();
+            (left.parse::<i32>().unwrap(), right.parse::<i32>().unwrap())
+        })
+        .unzip();
+
+    list1.sort();
+    list2.sort();
+
+    let part1: i32 = list1
+        .iter()
+        .zip(list2.iter())
+        .map(|(left, right)| (left - right).abs() )
+        .sum();
+
+    println!("part1: {}", part1);
+    assert!(part1 == 3574690);
+
+    let mut part2 = 0;
+    for left in list1.iter() {
+        for right in list2.iter() {
+            if left == right {
+                part2 += left;
+            }
+        }
+    }
+
+    println!("part2: {}", part2);
+    assert!(part2 == 22565391);
 }
--- 2024/day02/main.rs
+++ 2024/day02/main.rs
@@ -1,3 +1,55 @@
+fn check_safe(report: &Vec<i32>) -> bool {
+    let mut prev_increasing = report[1] > report[0];
+    let mut prev_reading = report[0];
+    for i in 1..report.len() {
+        let reading = report[i];
+        let diff = (reading - prev_reading).abs();
+        if diff < 1 || diff > 3 {
+            return false;
+        }
+        let increasing = reading > prev_reading;
+        if increasing != prev_increasing {
+            return false;
+        }
+        prev_reading = reading;
+        prev_increasing = increasing;
+    }
+    return true;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let reports: Vec<Vec<i32>> = include_str!("input")
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|num| num.parse::<i32>().unwrap())
+                .collect()
+        })
+        .collect();
+
+    let all_reports: Vec<Vec<Vec<i32>>> = reports
+        .iter()
+        .map(|report| {
+            (0..report.len())
+                .map(|i| {
+                    let mut dampened_report = report.clone();
+                    dampened_report.remove(i);
+                    dampened_report
+                })
+                .collect()
+        })
+        .collect();
+
+    let part1 = reports.iter().filter(|report| check_safe(report)).count();
+
+    println!("part1: {}", part1);
+    assert!(part1 == 686);
+
+    let part2 = all_reports
+        .iter()
+        .filter(|dampened_reports| dampened_reports.iter().any(|report| check_safe(report)))
+        .count();
+
+    println!("part2: {}", part2);
+    assert!(part2 == 717);
 }
